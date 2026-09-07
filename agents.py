95aec5ca70a0e5d6d2bcd8694fa093a61c26f7a8
gurdevsinghvip@gmail.com <gurdevsinghvip@gmail.com>
2026-09-07 14:52:23 +0530

    Add agents for research writing and critique functionality

---
+++ agents.py
@@ -0,0 +1,79 @@
+from langchain.agents import create_agent
+from langchain_mistralai import ChatMistralAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from tools import web_search, url_scraper
+
+from dotenv import load_dotenv
+load_dotenv()
+
+llm = ChatMistralAI(
+    model = "open-mistral-nemo",
+    temprature = 0
+)
+
+# agent 1 search agent 
+def build_serch_agent():
+    agent = create_agent(
+        model = llm,
+        tools = [web_search],
+    )
+
+    return agent
+
+# agent 2 url reader agent
+def build_url_reader_agent():
+    agent = create_agent(
+        model = llm,
+        tools = [url_scraper],
+    )
+
+    return agent
+
+# writer_chain
+writer_prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are an expert research writer. Write clear, structured and insightful reports."),
+    ("human", """Write a detailed research report on the topic below.
+
+Topic: {topic}
+
+Research Gathered:
+{research}
+
+Structure the report as:
+- Introduction
+- Key Findings (minimum 3 well-explained points)
+- Conclusion
+- Sources (list all URLs found in the research)
+
+Be detailed, factual and professional."""),
+])
+
+writer_chain = writer_prompt | llm | StrOutputParser()
+
+#critic_chain 
+
+critic_prompt = ChatPromptTemplate.from_messages([
+     ("system", "You are a sharp and constructive research critic. Be honest and specific."),
+    ("human", """Review the research report below and evaluate it strictly.
+
+Report:
+{report}
+
+Respond in this exact format:
+
+Score: X/10
+
+Strengths:
+- ...
+- ...
+
+Areas to Improve:
+- ...
+- ...
+
+One line verdict:
+..."""),
+])
+
+critic_chain = critic_prompt | llm | StrOutputParser()
